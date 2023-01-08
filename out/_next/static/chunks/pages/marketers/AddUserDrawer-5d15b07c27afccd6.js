fcf52b78be4f6d5a0a9eff2c77d83dc4bbf29d52
sara hakimi <sarahakimi2000@yahoo.com>
2023-01-08 16:26:10 +0330

    build after marketers page

---
+++ out/_next/static/chunks/pages/marketers/AddUserDrawer-5d15b07c27afccd6.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4147],{97368:function(n,_,r){(window.__NEXT_P=window.__NEXT_P||[]).push(["/marketers/AddUserDrawer",function(){return r(43279)}])}},function(n){n.O(0,[2283,1524,9075,2040,3279,9774,2888,179],(function(){return _=97368,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
