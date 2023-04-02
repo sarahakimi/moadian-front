ab49d1296834c6ddacbb42cdef804f9561213d69
sara hakimi <sarahakimi2000@yahoo.com>
2023-04-02 16:40:38 +0330

    settings page error handleing ok

--- src/pages/settings/TabAccount.js
+++ src/pages/settings/TabAccount.js
@@ -14,7 +14,7 @@ import {Controller, useForm} from "react-hook-form";
 import FormControl from "@mui/material/FormControl";
 import FormHelperText from "@mui/material/FormHelperText";
 import * as yup from "yup";
-import {Autocomplete} from "@mui/material";
+import {Autocomplete, Typography} from "@mui/material";
 import {ostan, shahr} from "iran-cities-json";
 import * as tus from "tus-js-client";
 import toast from "react-hot-toast";
@@ -70,7 +70,7 @@ const schema = yup.object().shape({
 
 function TabAccount() {
   const imgSrc = ''
-
+const [hasHub,setHasHub]= useState(false)
   const [imageUrl, setImageUrl] = useState("")
   const [resetImageUrl, setResetImageUrl] = useState("")
   const hub = useAuth().user.hub_id
@@ -118,12 +118,14 @@ function TabAccount() {
   useEffect(() => {
     fetchData().then(response => {
       if (response.data === null) {
+        setHasHub(false)
         reset(emptyForm)
       } else {
         reset(response.data)
         setImageUrl(response.data.image)
         setResetImageUrl(response.data.image)
         setSelectedSenderOstan(response.data.provence)
+        setHasHub(true)
       }
     }).catch((err) => {
       const errorMessage = err.response.data.message ? err.response.data.message : "خطایی رخ داده است"
@@ -176,175 +178,179 @@ function TabAccount() {
 
   return (
     <CardContent>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Grid container spacing={6}>
-          <Grid item xs={12} sx={{my: 5}}>
-            <Box sx={{display: 'flex', alignItems: 'center'}}>
-              <ImgStyled src={imageUrl === "" ? imgSrc : imageUrl} alt='Profile Pic'/>
-              <Box>
-                <ButtonStyled component='label' variant='contained' htmlFor='account-settings-upload-image'>
-                  بارگذاری عکس
-                  <input
-                    hidden
-                    type='file'
-                    onChange={handleSetUpload}
-                    accept='image/png, image/jpeg'
-                    id='account-settings-upload-image' name="image"
-                  />
-                </ButtonStyled>
-                <ResetButtonStyled color='error' variant='outlined'
-                                   onClick={() => setImageUrl(resetImageUrl === "" ? imgSrc : resetImageUrl)}>
-                  انصراف
-                </ResetButtonStyled>
+      {hasHub?
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Grid container spacing={6}>
+            <Grid item xs={12} sx={{my: 5}}>
+              <Box sx={{display: 'flex', alignItems: 'center'}}>
+                <ImgStyled src={imageUrl === "" ? imgSrc : imageUrl} alt='Profile Pic'/>
+                <Box>
+                  <ButtonStyled component='label' variant='contained' htmlFor='account-settings-upload-image'>
+                    بارگذاری عکس
+                    <input
+                      hidden
+                      type='file'
+                      onChange={handleSetUpload}
+                      accept='image/png, image/jpeg'
+                      id='account-settings-upload-image' name="image"
+                    />
+                  </ButtonStyled>
+                  <ResetButtonStyled color='error' variant='outlined'
+                                     onClick={() => setImageUrl(resetImageUrl === "" ? imgSrc : resetImageUrl)}>
+                    انصراف
+                  </ResetButtonStyled>
+                </Box>
               </Box>
-            </Box>
-          </Grid>
+            </Grid>
 
-          <Grid item xs={12} sm={6}>
-            <FormControl fullWidth sx={{mb: 4}}>
-              <Controller
-                name='name'
-                control={control}
-                rules={{required: true}}
-                render={({field: {value, onChange, onBlur}}) => (
-                  <TextField
-
-                    label='نام هاب'
-                    value={value}
-                    onBlur={onBlur}
-                    onChange={onChange}
-                    error={Boolean(errors.name)}
-                    InputLabelProps={{shrink: true}}
-                  />
-                )}
-              />
-              {errors.name && <FormHelperText sx={{color: 'error.main'}}>{errors.name.message}</FormHelperText>}
-            </FormControl>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <FormControl fullWidth sx={{mb: 4}}>
-              <Controller
-                name='fax'
-                control={control}
-                rules={{required: true}}
-                render={({field: {value, onChange, onBlur}}) => (
-                  <TextField
-
-                    label='فکس'
-                    value={value}
-                    onBlur={onBlur}
-                    onChange={onChange}
-                    error={Boolean(errors.fax)}
-
-                    inputProps={{maxLength: 12}}
-                    InputLabelProps={{shrink: true}}
-                  />
-                )}
-              />
-              {errors.fax && <FormHelperText sx={{color: 'error.main'}}>{errors.fax.message}</FormHelperText>}
-            </FormControl>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <FormControl fullWidth sx={{mb: 4}}>
-              <Controller
-                name='telephone'
-                control={control}
-                rules={{required: true}}
-                render={({field: {value, onChange, onBlur}}) => (
-                  <TextField
-
-                    label='تلفن'
-                    value={value}
-                    onBlur={onBlur}
-                    onChange={onChange}
-                    error={Boolean(errors.telephone)}
-                    inputProps={{maxLength: 11}}
-                    placeholder='021*******'
-                    dir='ltr'
-                    InputLabelProps={{shrink: true}}
-                  />
-                )}
-              />
-              {errors.telephone &&
-                <FormHelperText sx={{color: 'error.main'}}>{errors.telephone.message}</FormHelperText>}
-            </FormControl>
-          </Grid>
-          <Grid item xs={12} sm={6}> <FormControl fullWidth sx={{mb: 4}}>
-            <Controller
-              fullWidth
-              name='provence'
-              control={control}
-              rules={{required: true}}
-              render={({field: {onChange, value, onBlur}}) => (
-                <Autocomplete
-                  onBlur={onBlur}
-                  select
-                  options={ostan.map(element => element.name)}
-                  onChange={(event, values, value) => onChangeSenderOstan(event, onChange, values, value)}
-                  value={value}
-                  inputValue={value}
-                  disableClearable
-                  renderInput={params => (
+            <Grid item xs={12} sm={6}>
+              <FormControl fullWidth sx={{mb: 4}}>
+                <Controller
+                  name='name'
+                  control={control}
+                  rules={{required: true}}
+                  render={({field: {value, onChange, onBlur}}) => (
                     <TextField
-                      /* eslint-disable-next-line react/jsx-props-no-spreading */
-                      {...params}
-                      label='استان'
-                      variant='outlined'
-                      onChange={onChange}
-                      error={Boolean(errors.provence)}
 
-                    />
-
-                  )}
-                />
-              )}
-            />
-            {errors.provence && (
-              <FormHelperText sx={{color: 'error.main'}}>{errors.provence.message}</FormHelperText>
-            )}
-          </FormControl></Grid>
-          <Grid item xs={12} sm={6}> <FormControl fullWidth sx={{mb: 4}}>
-            <Controller
-              name='city'
-              control={control}
-              rules={{required: true}}
-              render={({field: {value, onChange, onBlur}}) => (
-                <Autocomplete
-                  onBlur={onBlur}
-                  select
-                  disableClearable
-                  options={shahr
-                    .filter(element => element.ostan === selectedSenderOstan)
-                    .map(element => element.name)}
-                  onChange={(event, values) => onChange(values)}
-                  value={value}
-                  noOptionsText="چیزی پیدا نشد"
-                  renderInput={params => (
-                    <TextField
-                      /* eslint-disable-next-line react/jsx-props-no-spreading */
-                      {...params}
-                      label='شهر'
-                      variant='outlined'
+                      label='نام هاب'
+                      value={value}
+                      onBlur={onBlur}
                       onChange={onChange}
-                      error={Boolean(errors.city)}
+                      error={Boolean(errors.name)}
+                      InputLabelProps={{shrink: true}}
                     />
                   )}
                 />
+                {errors.name && <FormHelperText sx={{color: 'error.main'}}>{errors.name.message}</FormHelperText>}
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <FormControl fullWidth sx={{mb: 4}}>
+                <Controller
+                  name='fax'
+                  control={control}
+                  rules={{required: true}}
+                  render={({field: {value, onChange, onBlur}}) => (
+                    <TextField
+
+                      label='فکس'
+                      value={value}
+                      onBlur={onBlur}
+                      onChange={onChange}
+                      error={Boolean(errors.fax)}
+
+                      inputProps={{maxLength: 12}}
+                      InputLabelProps={{shrink: true}}
+                    />
+                  )}
+                />
+                {errors.fax && <FormHelperText sx={{color: 'error.main'}}>{errors.fax.message}</FormHelperText>}
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <FormControl fullWidth sx={{mb: 4}}>
+                <Controller
+                  name='telephone'
+                  control={control}
+                  rules={{required: true}}
+                  render={({field: {value, onChange, onBlur}}) => (
+                    <TextField
+
+                      label='تلفن'
+                      value={value}
+                      onBlur={onBlur}
+                      onChange={onChange}
+                      error={Boolean(errors.telephone)}
+                      inputProps={{maxLength: 11}}
+                      placeholder='021*******'
+                      dir='ltr'
+                      InputLabelProps={{shrink: true}}
+                    />
+                  )}
+                />
+                {errors.telephone &&
+                  <FormHelperText sx={{color: 'error.main'}}>{errors.telephone.message}</FormHelperText>}
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={6}> <FormControl fullWidth sx={{mb: 4}}>
+              <Controller
+                fullWidth
+                name='provence'
+                control={control}
+                rules={{required: true}}
+                render={({field: {onChange, value, onBlur}}) => (
+                  <Autocomplete
+                    onBlur={onBlur}
+                    select
+                    options={ostan.map(element => element.name)}
+                    onChange={(event, values, value) => onChangeSenderOstan(event, onChange, values, value)}
+                    value={value}
+                    inputValue={value}
+                    disableClearable
+                    renderInput={params => (
+                      <TextField
+                        /* eslint-disable-next-line react/jsx-props-no-spreading */
+                        {...params}
+                        label='استان'
+                        variant='outlined'
+                        onChange={onChange}
+                        error={Boolean(errors.provence)}
+
+                      />
+
+                    )}
+                  />
+                )}
+              />
+              {errors.provence && (
+                <FormHelperText sx={{color: 'error.main'}}>{errors.provence.message}</FormHelperText>
               )}
-            />
-            {errors.city && (
-              <FormHelperText sx={{color: 'error.main'}}>{errors.city.message}</FormHelperText>
-            )}
-          </FormControl></Grid>
+            </FormControl></Grid>
+            <Grid item xs={12} sm={6}> <FormControl fullWidth sx={{mb: 4}}>
+              <Controller
+                name='city'
+                control={control}
+                rules={{required: true}}
+                render={({field: {value, onChange, onBlur}}) => (
+                  <Autocomplete
+                    onBlur={onBlur}
+                    select
+                    disableClearable
+                    options={shahr
+                      .filter(element => element.ostan === selectedSenderOstan)
+                      .map(element => element.name)}
+                    onChange={(event, values) => onChange(values)}
+                    value={value}
+                    noOptionsText="چیزی پیدا نشد"
+                    renderInput={params => (
+                      <TextField
+                        /* eslint-disable-next-line react/jsx-props-no-spreading */
+                        {...params}
+                        label='شهر'
+                        variant='outlined'
+                        onChange={onChange}
+                        error={Boolean(errors.city)}
+                      />
+                    )}
+                  />
+                )}
+              />
+              {errors.city && (
+                <FormHelperText sx={{color: 'error.main'}}>{errors.city.message}</FormHelperText>
+              )}
+            </FormControl></Grid>
 
 
-          <Grid item xs={12}>
-            <Button variant='contained' type="submit" sx={{mr: 4}}>
-              ذخیره تغییرات
-            </Button>
+            <Grid item xs={12}>
+              <Button variant='contained' type="submit" sx={{mr: 4}}>
+                ذخیره تغییرات
+              </Button>
+            </Grid>
           </Grid>
-        </Grid>
-      </form>
+        </form>:
+        <Typography variant="p">شما در هاب عضو نیستید. در صورتی که از عوضیت خود در هاب اطمینان دارید مجدد تلاش کنید.</Typography>
+      }
+
 
     </CardContent>
   )
