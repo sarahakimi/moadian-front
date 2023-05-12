32b568141b2e3984222e376f23ea377f1639e50a
sara hakimi <sarahakimi2000@yahoo.com>
2023-05-12 15:36:42 +0330

    bugs fixed and color pallete changed

---
+++ out/_next/static/chunks/pages/tracking-3d0d8fa67b56075d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[8337],{1878:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/tracking",function(){return u(82750)}])}},function(n){n.O(0,[3978,8521,2750,9774,2888,179],(function(){return _=1878,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
